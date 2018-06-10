d0d430388db22ca440a8186c1d78bbc088bf6348
ModoranDenisa <denisaa_95@yahoo.com>
2018-06-10 18:41:37 +0300

    Add files via upload

--- script/contactUs.php
+++ script/contactUs.php
@@ -4,26 +4,30 @@ session_start();
 
 if(isset($_POST['submit'])){
 
-    include_once 'script/dbHandler.php';
+    include_once 'dbHandler.php';
 
 
     $fname=mysqli_real_escape_string($conn, $_POST['fname']);
     $lname=mysqli_real_escape_string($conn, $_POST['lname']);
     $subject=mysqli_real_escape_string($conn, $_POST['subject']);
+    $country=mysqli_real_escape_string($conn, $_POST['country']);
 
 if(empty($fname) ||empty($lname) || empty($subject)){
         header("Location: ../contact_us.php?submitCheck=empty");
         exit();
-    }else{
-         
-            if((!preg_match("/^[a-zA-Z\s]*$/",$fname) || (!preg_match("/^[a-zA-Z\s]*$/",$lname) || (!preg_match("/^[a-zA-Z\s]*$/",$subject))
-            {
-                header("Location: ../contact_us.php?submitCheck=errorInvalidChar");
-                exit();
-            }            
-          }
-}   else{
-        header("Location: ../register.php?submitCheck=success") ;
+    }
+   else{
+                    $sql="INSERT INTO contactus (firstName,lastName,country,message) VALUES (?,?,?,?);";
+                    $stmt = mysqli_stmt_init($conn);
+
+                    if(!mysqli_stmt_prepare($stmt,$sql)){
+                        echo"SQL signUp statement failed";
+                    }else{
+                        mysqli_stmt_bind_param($stmt,"ssss", $fname,$lname,$country,$subject);
+                        mysqli_stmt_execute($stmt);
+        header("Location: ../contact_us.php?submitCheck=success") ;
         exit();
     }
+}
+}
 ?>
