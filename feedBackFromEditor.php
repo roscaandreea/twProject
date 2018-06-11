126611429d2dfc8bacd9f147f1d0b6ecd6d7905a
ModoranDenisa <denisaa_95@yahoo.com>
2018-06-11 18:26:51 +0300

    Add files via upload

--- feedBackFromEditor.php
+++ feedBackFromEditor.php
@@ -1,11 +1,14 @@
 <?php
     include_once 'Header.php';
+    include_once 'script/dbHandler.php';
+
+      $temp=$_POST['idevent'];
 ?>
 
 
 <section id="firstSectionInvitation">
     <div id="formDiv">
-       <form class="feedback" method="POST" action="script/feedbackEditor.php">
+       <form  method="POST" action="script/feedbackEditor.php">
                 <h1>Feedback Form Creator</h1>
                
                 
@@ -17,18 +20,25 @@
                   </label>
 
                   <label><b>Answer Type</b>
-                <select >
-                   
-                    <option>Add a question that should be answered as a text .</option>
+                  <?php
+                    echo '<select name="typeanswer">
+                    <option>Add a question that should be answered as a text.</option>
                     <option>Add a question that should be answered by choosing from multiple choices.</option>
                     <option>Add a question that should be answered with yes or no.</option>
                     <option>Add a question that should be answered on a scale from 1 to 5.</option>
-                </select>
-            </label>
-                    
-                <button type="Add Question" value="submit">Add Question</button>
+                    </select>'
+
+                  ?>
+                  </label>
+               
+               
+                
 
 
+
+                <?php   
+                echo '<button type="Add Question" name="submit" value="'.$temp.'">Add Question</button>';             
+                ?>
 
 
            <?php
