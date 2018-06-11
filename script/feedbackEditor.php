9998505e67d5c425ff8646b67d6f2dd05075ff8c
ModoranDenisa <denisaa_95@yahoo.com>
2018-06-11 18:27:41 +0300

    Add files via upload

--- script/feedbackEditor.php
+++ script/feedbackEditor.php
@@ -4,27 +4,37 @@
 
 if(isset($_POST['submit'])){
 
-    include_once 'dbHandler.php';
+    include_once 'dbHandler.php';    
+    $idevent=$_POST['submit'];
 
 
     $question=mysqli_real_escape_string($conn, $_POST['question']);
-    $idevent=$_POST['idevent'];
+    $answertype=mysqli_real_escape_string($conn, $_POST['typeanswer']);
 
-    if(empty($question) ||empty($answertype)){
+    $questionid="SELECT questionType_id FROM questiontypes WHERE questionType='$answertype'";
+
+
+   if(empty($question)){
         header("Location: ../feedBackFromEditor.php?addqu=empty");
         exit();
-    }else{
-         
-            if(!preg_match("/^[a-zA-Z\s]*$/",$question))
-            {
-                header("Location: ../feedBackFromEditor.php?addqu=errorInvalidChar");
-                exit();
-            }            
-          }
-}   else{
+    }
+   else{
+                   $sql="INSERT INTO formquestions (events_id,question,questionType_id) VALUES (?,?,?);";
+     
+                    $stmt = mysqli_stmt_init($conn);
+
+                    if(!mysqli_stmt_prepare($stmt,$sql)){
+                     echo"SQL feedback statement failed";
+                   }else{
+                     mysqli_stmt_bind_param($stmt,"sss", $idevent,$question,$questionid);mysqli_stmt_bind_param($stmt,"sss", $idevent,$question,$questionid);
+                    
+                     mysqli_stmt_execute($stmt);
         header("Location: ../feedBackFromEditor.php?addqu=success") ;
         exit();
     }
-    print_r($idevent);
+}
+//print_r($idevent);
 
+
+}
 ?>
