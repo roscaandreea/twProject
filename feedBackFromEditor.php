3f9c7fa05e7e5ecc14fda13308e9e6ce66890ad3
ModoranDenisa <denisaa_95@yahoo.com>
2018-06-13 16:01:57 +0300

    Add files via upload

--- feedBackFromEditor.php
+++ feedBackFromEditor.php
@@ -1,14 +1,17 @@
 <?php
     include_once 'Header.php';
     include_once 'script/dbHandler.php';
-
-      $temp=$_POST['idevent'];
+    $temp=$_POST['idevent'];
+      
 ?>
 
 
 <section id="firstSectionInvitation">
     <div id="formDiv">
-       <form  method="POST" action="script/feedbackEditor.php">
+       <form   action="script/feedbackEditor.php" method="POST">
+
+
+
                 <h1>Feedback Form Creator</h1>
                
                 
@@ -22,45 +25,17 @@
                   <label><b>Answer Type</b>
                   <?php
                     echo '<select name="typeanswer">
-                    <option>Add a question that should be answered as a text.</option>
-                    <option>Add a question that should be answered by choosing from multiple choices.</option>
-                    <option>Add a question that should be answered with yes or no.</option>
-                    <option>Add a question that should be answered on a scale from 1 to 5.</option>
+                    <option value="1">Add a question that should be answered as a text.</option>
+                    <option value="2">Add a question that should be answered by choosing from multiple choices.</option>
+                    <option value="3">Add a question that should be answered with yes or no.</option>
+                    <option value="4">Add a question that should be answered on a scale from 1 to 5.</option>
                     </select>'
-
                   ?>
-                  </label>
+                  </label>       
                
-               
-                
-
-
-
                 <?php   
                 echo '<button type="Add Question" name="submit" value="'.$temp.'">Add Question</button>';             
                 ?>
-
-
-           <?php
-
-            if(!isset($_GET['addqu'])){
-                exit();
-            }else{
-                $submitCheck=$_GET['addqu'];
-
-                if($submitCheck == "empty"){
-                    echo "You did not fill all the fields!";
-                }else if($submitCheck=="errorInvalidChar"){
-                    echo "You have used invalid characters!";
-                    exit();
-                }else if($submitCheck=="succes"){
-                    echo "Your form was successfully submitted!";
-                }
-            }
-            
-            ?>
-
-                
         
         </form>
         
@@ -72,4 +47,5 @@
 
 <?php
     include_once 'Footer.php';
+   // print_r($temp);
 ?>
