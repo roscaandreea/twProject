425a5db4e9be48448f8f6e1b7a529f397d9f9483
Marin Mihai-Alexandru <marin.mihai1997@yahoo.com>
2018-06-14 03:44:00 +0300

    Revert "Merge branch 'master' of https://github.com/roscaandreea/twProject"
    This reverts commit 86d50be2ed662bb9d7a6dc69f6f4fddc91933367, reversing
    changes made to 3e9b268e6b3bf2c26f19a099ab66040bff68d2da.

--- Feedback.php
+++ Feedback.php
@@ -2,20 +2,13 @@
     <div id="formDiv">
 
 <?php
-    $currentEv=17;
     include_once 'Header.php';
     include_once 'script/dbHandler.php';
     //$temp=$_POST['idevent'];
-    $events="SELECT formQuestions_id,question, questionType_id FROM formquestions WHERE events_id=".$currentEv;
+    $events="SELECT formQuestions_id,question, questionType_id FROM formquestions WHERE events_id=17";
     $results=mysqli_query($conn,$events);
     $resultCheck=mysqli_num_rows($results);
     if($resultCheck>0){
-      echo'<form method="POST" action="script/receiveAnswers.php">
-      
-      <input type="hidden" name="currentEvent" value='.$currentEv.' ></input>
-      
-      ';
-      
         while($row=mysqli_fetch_assoc($results)){
             $question_id=$row['formQuestions_id'];
             $question=$row['question'];
@@ -23,44 +16,45 @@
            // print_r($row['questionType_id']);
             if($question_type==1)
             {
-              echo '<h4 id="question">'.$question.'
-              <div name='.$question_id.'>            
-              <input type="text" name="'.$question_id.'"  placeholder="Enter your response" >
-              </div>
-              </h4>';
+              
+              echo '<td id="question">'.$question.'
+             <form>
+             <br>                
+              <input type="text" name="response"  placeholder="Enter your response" ><br><br><br><br>
+              </form>
+              </td>';
+               
             }else 
                   if($question_type==2)
                   {
-                    echo '<h4 id="question">'.$question.'
-                      <div name='.$question_id.'> 
-                        <br>
-                        <input type="radio" name='.$question_id.' value="yes"> Yes<br>
-                        <input type="radio" name='.$question_id.'  value="no"> No
-                      </div>
-                      </h4>'
-                      ;
+                    echo '<td id="question">'.$question.'
+                      <form action="">
+                      <br>
+                       <input type="radio" name="Yes/No" value="yes"> Yes<br>
+                       <input type="radio" name="Yes/No"  value="no"> No
+               
+                      </form>
+                      </td>';
                   }else
                   if($question_type==3)
                   {
-                    echo '<h4 id="question">'.$question.'
-                      
-                    <div name='.$question_id.'>
-                        <select name='.$question_id.'>
+                    echo '<td id="question">'.$question.'
+                      <form>
+                      <br>
+                        <select>
                               <option>1</option>
                               <option>2</option>
                               <option>3</option>
                               <option>4</option>
                               <option>5</option>
-                        </select>
-                      </div>
-                      </h4>';
+                        </select><br><br><br><br>
+               
+                      </form>
+                      </td>';
                   }
 
              
-    }
-
-    echo '<button type="submit" name="submit">Finalize Questionaire</button></form>';
-  }
+    }}
 ?>
 </div>
 </section>
